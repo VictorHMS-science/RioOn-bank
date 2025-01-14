5b03a62d461f9158d94a39c98dde5f9842cf6fb3
Victor Hugo Martins de Souza <victormartins.souza@gmail.com>
2025-01-14 11:41:18 -0300

    RioOn Bank - Cópia da página inicial do Nubank

---
+++ src/Home/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#820AD1', // Cor de fundo roxa do Nubank
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF', // Branco
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#FFFFFF',
+  },
+});
+
+export default styles;
